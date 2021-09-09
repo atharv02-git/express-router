1415fbbdcf72b5f4da88e9540413337b6248a5dc
Atharv <atharvbhandare2001@gmail.com>
2021-09-09 22:10:38 +0530

    Added Middlewares

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
-const shelterRoutes = require('./routes/shelters')
-const dogRoutes = require('./routes/dogs')
+const shelterRoutes = require('./routes/shelters');
+const dogRoutes = require('./routes/dogs');
+const adminRoutes = require('./routes/admin')
 
-app.use('/shelter', shelterRoutes)
-app.use('/dogs', dogRoutes)
+app.use('/shelters', shelterRoutes);
+app.use('/dogs', dogRoutes);
+app.use('/admin', adminRoutes)
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+    console.log('Serving app on localhost:3000')
 })
